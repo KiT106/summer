3cd5c9c34020dc7bd1c6b005442fd5bff2ada6c9
Thang Nguyen <thang.nguyen@synamed.com>
2016-07-22 15:21:13 +0700

    Spring 3

--- src/main/java/io/dungdm93/summer/GreetingController.java
+++ src/main/java/io/dungdm93/summer/GreetingController.java
@@ -1,17 +1,25 @@
 package io.dungdm93.summer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+@Controller
+public class GreetingController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("msg", "Hello World");
+        return "greeting";
+    }
 
-public class GreetingController extends AbstractController {
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
-        ModelAndView model = new ModelAndView("greeting");
-        model.addObject("msg", "Hello World");
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("greeting");
+        model.addObject("msg", name);
 
         return model;
     }
